5b46e6ce19abbb117b129fef91d0721a95aeb47e
z-tim-ding <30423137+z-tim-ding@users.noreply.github.com>
2020-07-16 15:54:51 -0400

    add logic to generate book jsons by category

--- mtx/fixture_gen.py
+++ mtx/fixture_gen.py
@@ -4,6 +4,7 @@ import uuid
 
 WORK_DIR = "C:\\Users\\yoyon\\Desktop\\blinkist_project"
 categories, books = [], []
+book_count, error_count = 0, 0
 
 for dir_name in filter(lambda x: x.endswith("-en"), os.listdir(WORK_DIR)):
     category_name = dir_name[: -3].replace("-", " ").title()
@@ -23,11 +24,19 @@ for dir_name in filter(lambda x: x.endswith("-en"), os.listdir(WORK_DIR)):
         if book_name.lower().endswith(" en"):
             book_name = book_name[: -3]
         book_text = ""
-        with open(os.path.join(WORK_DIR, dir_name, book_html), "r", encoding="utf-8") as f:
-            try:
+        book_count += 1
+
+        try:
+            with open(os.path.join(WORK_DIR, dir_name, book_html), "r", encoding="cp1252") as f:
                 book_text = f.read()
-            except UnicodeDecodeError:
+        except UnicodeDecodeError as e:
+            try:
+                with open(os.path.join(WORK_DIR, dir_name, book_html), "r", encoding="utf-8") as f:
+                    book_text = f.read()
+            except UnicodeDecodeError as e:
                 print("Cannot decode book " + book_name)
+                print(str(e))
+                error_count += 1
 
         book = {
             "model": "catalog.Book",
@@ -40,8 +49,8 @@ for dir_name in filter(lambda x: x.endswith("-en"), os.listdir(WORK_DIR)):
         }
         books.append(book)
 
+    with open(os.path.join(os.path.abspath("../fixtures"), category_name + ".json"), "a+") as f:
+        f.write(json.dumps(books))
+
 with open(os.path.abspath("../fixtures/categories.json"), "w+") as f:
     f.write(json.dumps(categories))
-
-print(books)
-
