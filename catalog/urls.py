fc82cd029883cd917e6a03f3cadaa91fb487c6fd
z-tim-ding <30423137+z-tim-ding@users.noreply.github.com>
2020-07-22 21:51:14 -0400

    add code to support category list and category-book list

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='catalog_home'),
+    path('categories/', views.CategoryListView.as_view(), name='categories'),
+    path('category-books/<uuid:category_id>',
+         views.get_category_books,
+         name='category_books')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Book, Category
+from django.views import generic
+from django.shortcuts import get_list_or_404
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse("<h1>Book Categories</h1>")
+    num_categories = Category.objects.all().count()
+    num_books = Book.objects.all().count()
+    context = {
+        'num_books': num_books,
+        'num_categories': num_categories,
+    }
+    return render(request, 'index.html', context=context)
+
+
+def get_category_books(request, category_id):
+    book_list = get_list_or_404(Book, category_id=category_id)
+    return render(request, 'catalog/category_book_list.html', context={'book_list': book_list})
+
+
+class CategoryListView(generic.ListView):
+    model = Category
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['num_categories'] = Category.objects.all().count()
+        context['num_books'] = Book.objects.all().count()
+        return context
+
